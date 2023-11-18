c45934935e6184e9be81bc0b2dc97e03172e7146
Eldar <151397032+nekoLxver@users.noreply.github.com>
2023-11-18 23:50:23 +0300

    Add files via upload

---
+++ javascript/gallery.js
@@ -0,0 +1,187 @@
+let girls = [
+    {
+        girlImage: "images/gallerypage/megumin.jpg",
+        girlName: "Megumin",
+        girlDescr: "Megumin (めぐみん) is an Arch Wizard of the Crimson Demons in the Fantasy World, and is the first person to join Kazuma's party. She is one of the main characters of the KonoSuba series.",
+        girlWiki: "https://konosuba.fandom.com/wiki/Megumin",
+        girlGallery: ['https://w.forfun.com/fetch/36/36e447cca68536ac679f9e1e0dbf1d61.jpeg', 'https://nichinichichan.files.wordpress.com/2017/01/konosuba07.jpg', 'https://abrakadabra.fun/uploads/posts/2022-01/1643296128_5-abrakadabra-fun-p-avatarki-megumin-15.jpg', 'https://i.pinimg.com/originals/17/eb/0f/17eb0fe2e62be81cbb8c733ae83e32c7.jpg', 'https://celes.club/uploads/posts/2023-06/1688138070_celes-club-p-megumin-oboi-vkontakte-59.jpg', 'https://somoskudasai.com/wp-content/uploads/2022/07/megumin.jpg']
+    },
+    {
+        girlImage: "images/gallerypage/rem.jpg",
+        girlName: "Rem",
+        girlDescr: "Rem (レム) is a maid in service of Margrave Roswaal L Mathers alongside her sister, Ram. She is one of the main supporting characters of Re:Zero, with major roles in Arc 2, Arc 3, and Arc 7.",
+        girlWiki: "https://rezero.fandom.com/wiki/Rem",
+        girlGallery: ['https://collectabox.com/wp-content/uploads/2020/09/ReZERO-The-Prophecy-of-the-Throne.jpg', 'https://i.pinimg.com/originals/19/61/76/1961761ebae2d779bf86d9c7f9690b5e.jpg', 'https://nerdbear.com/wp-content/uploads/2022/12/Best-ISFJ-Anime-Characters-Rem.png', 'https://simstone.ru/assets/images/downloads/sims/stars/short-hair-anime-girls-re-zero-kara-hajimeru-isekai-seikatsu-anime-blue-hair-rem-re-zero-open-shirt-blue-eyes-247675.jpg', 'https://pics.animepicsx.net/images/z3w/ohc/e6cmbkzi1i.png', 'https://animesolution.com/wp-content/uploads/2020/03/ReZero-kara-Hajimeru-Isekai-Seikatsu-Shin-Henshuu-ban-10A_11.04_2020.03.09_18.29.36stitch.jpg']
+    },
+    {
+        girlImage: "images/gallerypage/akame.jpeg",
+        girlName: "Akame",
+        girlDescr: "Akame (アカメ) is the titular heroine, alongside Tatsumi, and lead anti-heroine of the manga series Akame ga Kill!, as well as the main protagonist of the prequel manga Akame ga Kill! Zero.",
+        girlWiki: "https://akamegakill.fandom.com/wiki/Akame",
+        girlGallery: ['https://w.forfun.com/fetch/c0/c00366a40a44d4af16073f7461349541.jpeg', 'https://cojo.ru/wp-content/uploads/2023/01/ubiitsa-akame-akame-1.webp', 'https://wallpapers.com/images/hd/akame-ga-kill-4k-3840-x-2160-wallpaper-eggvq4dpw5xqr3iq.jpg', 'https://www.free-wallpapers.su/data/media/3/big/anm7644.jpg', 'https://www.lenbaget.ru/wp-content/uploads/2021/11/23025-1000x830-1.jpg', 'https://kartinki.pibig.info/uploads/posts/2023-04/1681950945_kartinki-pibig-info-p-akame-kartinki-arti-krasivo-36.jpg']
+    },
+    {
+        girlImage: "images/gallerypage/hinata.jpg",
+        girlName: "Hinata",
+        girlDescr: "Hinata Uzumaki (うずまきヒナタ, Uzumaki Hinata, née Hyūga (日向)) is a kunoichi of Konohagakure. Formerly the heiress of the Hyūga clan, she lost the position upon being deemed unsuited for the responsibilities of leading the clan.",
+        girlWiki: "https://naruto.fandom.com/wiki/Hinata_Hyūga",
+        girlGallery: ['https://kartinki.pics/uploads/posts/2022-03/1646294571_43-kartinkin-net-p-kartinki-khinati-43.jpg', 'https://yt3.googleusercontent.com/xmayrF9t_RwBYxcsFlpBaeguZ5iW5u7MWdTi7J1Kt5_-p51_JoGPrnC4YC1E57qXrLVXqy2V=s900-c-k-c0x00ffffff-no-rj', 'https://assets.puzzlefactory.com/puzzle/277/530/original.jpg', 'https://celes.club/uploads/posts/2022-05/1652104039_22-celes-club-p-khinata-khyuga-art-krasivo-24.jpg', 'https://catherineasquithgallery.com/uploads/posts/2021-02/1612154669_23-p-khinata-na-fioletovom-fone-28.jpg', 'https://i.ytimg.com/vi/ZFEMmXvs12Q/maxresdefault.jpg']
+    },
+    {
+        girlImage: "images/gallerypage/suzune.jpg",
+        girlName: "Suzune",
+        girlDescr: "Suzune Horikita (堀ほり北きた 鈴すず音ね, Horikita Suzune) is one of the main characters of the You-Zitsu series and the main heroine of the √Horikita spin-off manga. She is known to be the Student Council president of the school on the 1st Year Arc.",
+        girlWiki: "https://you-zitsu.fandom.com/wiki/Suzune_Horikita",
+        girlGallery: ['https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEggOYuUP3hkywDP_mgKiV03US3I_pJVx-WBq1Fs0DJ17-PRearZzXw7IAzsnFJhAJgfX8pbX92s4aRTO80rwl5qvS1Fxf0AdQ96faBY1k3TvBbEWVrgOT-wrDFelCNJ3e39fZkSse9AY_MZK-HMraonXLD_c9JAQG9xGtE51xAYxsIRM8bcxw/s1100/yande.re%20965997%20sample.jpg', 'https://i.pinimg.com/originals/46/af/3e/46af3e546bf9a24935c2781a5f0cd3fe.jpg', 'https://i.pinimg.com/originals/e7/00/10/e70010ebcf143739d48ddbc2faf6b902.jpg', 'http://m.gettywallpapers.com/wp-content/uploads/2023/05/Suzune-Horikita-Wallpaper-2023.jpg', 'https://get.wallhere.com/photo/anime-anime-girls-Anime-screenshot-Youkoso-Jitsuryoku-Shijou-Shugi-no-Kyoushitsu-e-Horikita-Suzune-long-hair-black-hair-solo-artwork-digital-art-2181453.jpg', 'https://gen.jut.su/uploads/preview/110/0/0/4_1550824524.jpg']
+    },
+    {
+        girlImage: "images/gallerypage/violet.jpeg",
+        girlName: "Violet",
+        girlDescr: "Violet Evergarden (ヴァイオレット・エヴァーガーデン, Vaioretto Evāgāden?) is the protagonist of the Violet Evergarden series. Violet is a former soldier who was enlisted in the Leidenschaftlich army and fought in the war, where she was treated as nothing more than a weapon.",
+        girlWiki: "https://you-zitsu.fandom.com/wiki/Suzune_Horikita",
+        girlGallery: ['https://i.pinimg.com/originals/70/98/c0/7098c00edc29d8d6f35fe075abacd169.jpg', 'https://media.kg-portal.ru/anime/v/violetevergarden/production/violetevergarden_5.jpg', 'https://animepersona.com/wp-content/uploads/2022/12/1672063861055.jpg', 'https://besthqwallpapers.com/Uploads/8-4-2022/197504/violet-evergarden-japanese-manga-anime-characters-violet-evergarden-character-art.jpg', 'https://krot.club/uploads/posts/2022-01/1642552591_1-krot-info-p-vaiolet-evergarden-art-1.jpg', 'https://pibig.info/uploads/posts/2021-05/1619896128_16-pibig_info-p-violetta-evergarden-art-anime-krasivo-20.jpg']
+    },
+    {
+        girlImage: "images/gallerypage/yuigahama.jpeg",
+        girlName: "Yui",
+        girlDescr: "Yui Yuigahama (由比ヶ浜 結衣 Yuigahama Yui) is a student in Class 2F of Sōbu High School and the first official customer of the Service Club. She later becomes the third member of the Service Club.",
+        girlWiki: "https://oregairu.fandom.com/wiki/Yui_Yuigahama",
+        girlGallery: ['https://static.zerochan.net/Yuigahama.Yui.full.3450244.jpg', 'https://img5.goodfon.com/original/3508x2480/8/69/yui-yuigahama-yahari-ore-no-seishun-love-comedy-wa-machigatt.jpg', 'https://static.zerochan.net/Yuigahama.Yui.full.3452651.jpg', 'https://static.zerochan.net/Yuigahama.Yui.full.3706143.jpg', 'https://static.zerochan.net/Yuigahama.Yui.full.3067596.jpg', 'https://w.forfun.com/fetch/a0/a01443f722a82df83ce504c85558f129.jpeg']
+    },
+    {
+        girlImage: "images/gallerypage/erina.jpg",
+        girlName: "Erina",
+        girlDescr: `Erina Nakiri (薙な切きり えりな, Nakiri Erina) is one of the heroines and was sometimes a rival to Sōma Yukihira in Shokugeki no Soma. Wielding the best palate in the world, "God's Tongue" and a member of the illustrious Nakiri family.`,
+        girlWiki: "https://shokugekinosoma.fandom.com/wiki/Erina_Nakiri",
+        girlGallery: ['https://w.forfun.com/fetch/e8/e8ee80fc4e872c5825409e127bb35204.jpeg', 'https://w.forfun.com/fetch/65/65decca146849203c3e2c10182e08886.jpeg', 'https://konachan.net/sample/f65d2cd037c04df8a30f4bb522a7df2b/Konachan.com%2520-%2520338478%2520sample.jpg', 'https://i.pinimg.com/originals/88/ec/fa/88ecfaa3507d3c58a28b3852078c8678.jpg', 'https://w.forfun.com/fetch/d8/d88df5220397054d90c9a3bd1eaf2774.jpeg', 'https://i.pinimg.com/originals/12/b0/28/12b02854e6867c87533ee7dc06083afa.png']
+    },
+    {
+        girlImage: "images/gallerypage/chika.jpg",
+        girlName: "Chika",
+        girlDescr: `Chika Fujiwara (藤原 千花ふじわら ちか) is one of the protagonists in the Kaguya-sama wa Kokurasetai series. She was a third-year high school student at Shuchi'in Academy, the secretary of the 67th and 68th Student Council.`,
+        girlWiki: "https://kaguyasama-wa-kokurasetai.fandom.com/wiki/Chika_Fujiwara",
+        girlGallery: ['https://i.pinimg.com/736x/e6/d7/20/e6d72064bad54e2e4b5842f28d701278.jpg', 'https://w.forfun.com/fetch/11/117c1e34a4c2c95d51e84c9a64ab8f5a.jpeg', 'https://id-static.z-dn.net/files/d2b/ebb278448414db0a63818c5ea20e4439.jpg', 'https://i.pinimg.com/originals/d6/14/40/d61440e6b21e25b18e52189ac48269ff.jpg', 'https://w.forfun.com/fetch/42/42db9951426ae27fff5922cb1fffe8de.jpeg', 'http://i.kym-cdn.com/photos/images/facebook/001/458/670/b5c.jpg']
+    },
+    {
+        girlImage: "images/gallerypage/kiyoko.png",
+        girlName: "Kiyoko",
+        girlDescr: `Kiyoko Shimizu (Japanese: 清水しみず 潔子きよこ Shimizu Kiyoko) was a third-year student at Karasuno High, as well as one of the managers for the Karasuno boy's volleyball team for majority of the series.`,
+        girlWiki: "https://haikyuu.fandom.com/wiki/Kiyoko_Shimizu",
+        girlGallery: ['https://i.pinimg.com/736x/81/ec/4c/81ec4c108d9e53e2d48f6c4cdfe93a34.jpg', 'https://i.pinimg.com/originals/4c/c6/61/4cc6618923029c6503812412c5a901a5.jpg', 'https://i.pinimg.com/originals/6b/6e/91/6b6e915533d9cda796db5c7e760d72f9.jpg', 'https://i.pinimg.com/originals/d1/80/6d/d1806d0ef07c3827b6598f8c158e5acb.jpg', 'https://i.pinimg.com/736x/d6/8f/05/d68f0577ffebdc10dff4b7f166c1e89b.jpg', 'https://i.pinimg.com/originals/74/9b/6e/749b6e03d0d5a853fd4faca4116bc26c.png']
+    },
+    {
+        girlImage: "images/gallerypage/Maki.jpg",
+        girlName: "Maki",
+        girlDescr: `Maki Zenin (禪ぜん院いん真ま希き Zen'in Maki?) is a major supporting character in Jujutsu Kaisen and one of the main protagonists of its prequel, Jujutsu Kaisen 0: Jujutsu High. She is currently a second-year at Tokyo Jujutsu High.`,
+        girlWiki: "https://jujutsu-kaisen.fandom.com/wiki/Maki_Zenin",
+        girlGallery: ['https://w.forfun.com/fetch/d5/d5410c1ce53f3dc5f988596b1e496d83.jpeg', 'https://i.pinimg.com/originals/6b/67/6f/6b676fa469772f0f8ba30ef21454eca1.jpg', 'https://cdni.fancaps.net/file/fancaps-animeimages/13778140.jpg', 'https://cs14.pikabu.ru/post_img/big/2022/04/18/9/1650296471182548440.png', 'https://chillywilly.club/uploads/posts/2023-03/1678602836_chillywilly-club-p-maki-zenin-magicheskaya-bitva-art-pinteres-10.jpg', 'https://staticg.sportskeeda.com/editor/2022/07/1fef5-16581803983970.png']
+    },
+    {
+        girlImage: "images/gallerypage/mirai.jpg",
+        girlName: "Mirai",
+        girlDescr: `Mirai Kuriyama (栗山 未来, Kuriyama Mirai ) is the deuteragonist of Kyoukai no Kanata, she's a first-year student and a member of the school's Literature Club. Mirai comes from a bloodline of Spirit World Warriors.`,
+        girlWiki: "https://kyoukainokanata.fandom.com/wiki/Mirai_Kuriyama",
+        girlGallery: ['http://blog-imgs-61.fc2.com/y/a/r/yaraon/1383147040214.jpg', 'https://imgix.ranker.com/user_node_img/50056/1001101092/original/mirai-kuriyama-u3', 'https://w.forfun.com/fetch/d3/d3a97ddaa4ae4d3b82fdc0025b3ccac1.jpeg', 'https://i.pinimg.com/736x/5c/17/c0/5c17c0a86b0f0ac1b53c89c2e73213dd.jpg', 'https://i.artfile.ru/1920x1130_1164717_[www.ArtFile.ru].jpg', 'https://w.forfun.com/fetch/9f/9f38a58cc7fd635d307f772c7e913e77.jpeg?w=1470&r=0.5625']
+    },
+    {
+        girlImage: "images/gallerypage/rin.jpg",
+        girlName: "Rin",
+        girlDescr: `Rin Tohsaka (遠坂 凛, Tōsaka Rin?) is one of the three main heroines of Fate/stay night who acts as the Master of Archer in the Fifth Holy Grail War. She is the current head of the Tohsaka family and the older sister of Sakura Matou.`,
+        girlWiki: "https://typemoon.fandom.com/wiki/Rin_Tohsaka",
+        girlGallery: ['https://adonius.club/uploads/posts/2022-02/1645473758_1-adonius-club-p-tosaka-rin-art-1.jpg', 'https://img4.goodfon.ru/wallpaper/nbig/1/9f/fate-stay-night-rin-devushka-parni.jpg', 'https://phonoteka.org/uploads/posts/2021-07/1625677574_3-phonoteka-org-p-rin-tosaka-art-etti-krasivo-5.jpg', 'https://kartinkin.net/uploads/posts/2022-11/thumbs/1668502805_kartinkin-net-p-rin-tosaka-art-pinterest-44.jpg', 'https://w.forfun.com/fetch/33/33d89bea4f3f20da3aff6a326ae5460d.jpeg', 'https://i.pinimg.com/originals/ce/a3/56/cea356e776db8d48065424730eec9744.jpg']
+    },
+    {
+        girlImage: "images/gallerypage/kikyou.jpeg",
+        girlName: "Kikyou",
+        girlDescr: `Kikyō Kushida (櫛くし田だ 桔き梗きょう, Kushida Kikyō) is one of the main characters of the You-Zitsu series. She is a student of Class 1-D. She is quite popular in her class as well as the whole school as she aims to befriend everyone.`,
+        girlWiki: "https://you-zitsu.fandom.com/wiki/Kikyō_Kushida",
+        girlGallery: ['https://static.animecorner.me/2022/12/kikyo.jpg', 'http://m.gettywallpapers.com/wp-content/uploads/2023/05/Kikyou-Kushida-Wallpaper.jpg', 'https://static.animecorner.me/2022/08/Classroom-of-the-Elite-season-2-Episode-9-preview-image-5.jpg', 'https://static.animecorner.me/2022/08/classroom-of-the-elite-season-2-episode-6-3.jpg', 'https://i.ytimg.com/vi/yT0NuGinS00/maxresdefault.jpg', 'https://cdn.myanimelist.net/s/common/forum/752fc003-6c6c-4857-9861-77f5032e553b.jpeg']
+    },
+    {
+        girlImage: "images/gallerypage/mai.jpeg",
+        girlName: "Mai",
+        girlDescr: `Mai Sakurajima (桜島 麻衣, Sakurajima Mai) is the main heroine of the Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai series. She is a notable actress/model and a third year at Minegahara High School.`,
+        girlWiki: "https://aobuta.fandom.com/wiki/Mai_Sakurajima",
+        girlGallery: ['http://static.minitokyo.net/downloads/45/02/740145.jpg', 'https://kartinki.pics/uploads/posts/2021-07/1625536995_12-kartinkin-com-p-anime-glupii-svin-anime-krasivo-12.jpg', 'https://i.pinimg.com/originals/86/f6/2d/86f62dbf56ec42da9c0e113930656ac7.jpg', 'https://w.forfun.com/fetch/47/47e6b10f7794a5d2a01ad352ff22a54b.jpeg', 'https://i.pinimg.com/originals/c9/df/be/c9dfbebc18d0989bc6ef69d3880e736f.jpg', 'https://w.forfun.com/fetch/f3/f35978011d983b6cef0bf561750b0813.jpeg?w=1470&r=0.5625']
+    },
+];
+
+let main = document.querySelector("main");
+let socialsBtn = document.querySelector(".socials-hover")
+let socialsWindow = document.querySelector(".socials");
+let gridRow = document.querySelector(".row");
+
+socialsBtn.addEventListener("click", function() {
+
+    socialsBtn.style.pointerEvents = "none";
+    main.style.opacity = 0.8;
+    socialsWindow.style.display = "flex";
+    gridRow.style.pointerEvents = "none";
+    let socialsCloseBtn = document.querySelector(".btn-close");
+    socialsCloseBtn.addEventListener("click", function() {
+        socialsWindow.style.display = "none";
+        main.style.opacity = 1;
+        gridRow.style.pointerEvents = "auto";
+        socialsBtn.style.pointerEvents = "auto";
+    })
+
+})
+
+gridRow.innerHTML = "";
+
+for (let girl of girls) {
+    gridRow.insertAdjacentHTML("beforeend", `
+    <div class="col-lg-4">
+        <div class="card text-white mb-3" style="width: 300;">
+            <img src=${girl.girlImage} class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${girl.girlName}</h5>
+                <p class="card-text">${girl.girlDescr}</p>
+                <a href="#" class="btn btn-danger">Images</a>
+                <a target="_blank"  href="${girl.girlWiki}" class="btn btn-primary">Wiki</a>
+            </div>
+        </div>
+    </div>
+    `);
+}
+
+let imageButtons = document.querySelectorAll(".btn-danger");
+let galleryRow = document.querySelector(".row");
+let galleryWindow;
+let closeGallery;
+
+for (let button of imageButtons) {
+    button.addEventListener("click", function showImages(e) {
+
+        galleryRow.style.opacity = 0.8;
+        socialsBtn.style.pointerEvents = "none";
+        e.preventDefault();
+        galleryRow.style.pointerEvents = "none";
+        let nameOfWaifu = button.parentNode.firstElementChild.textContent;
+        let [currentWaifu] = girls.filter((obj) => obj.girlName === nameOfWaifu);
+        main.insertAdjacentHTML("beforeend", `
+        <div class="images-popup-window">
+            <button class="btn btn-close btnCloseGallery"></button>
+            <img src="${currentWaifu.girlGallery[0]}" alt="" class="popup-image">
+            <img src="${currentWaifu.girlGallery[1]}" alt="" class="popup-image">
+            <img src="${currentWaifu.girlGallery[2]}" alt="" class="popup-image">
+            <img src="${currentWaifu.girlGallery[3]}" alt="" class="popup-image">
+            <img src="${currentWaifu.girlGallery[4]}" alt="" class="popup-image">
+            <img src="${currentWaifu.girlGallery[5]}" alt="" class="popup-image">
+        </div>
+        `);
+        galleryWindow = document.querySelector(".images-popup-window");
+        closeGallery = document.querySelector(".btnCloseGallery");
+        console.log(closeGallery);
+        closeGallery.addEventListener("click", function() {
+            console.log(closeGallery);
+            galleryWindow.remove();
+            galleryRow.style.pointerEvents = "auto";
+            socialsBtn.style.pointerEvents = "auto";
+            galleryRow.style.opacity = 1;
+        })
+        
+    });
+}
+
+
---
+++ javascript/script.js
@@ -0,0 +1,195 @@
+let waifus = [
+    ["images/mainpage/waify/horikita.jpg", "Suzune"],
+    ['images/mainpage/waify/kushida.jpg', "Kikyou"],
+    ['images/mainpage/waify/mai.jpg', "Mai"],
+    ['images/mainpage/waify/megumin.jpg', "Megumin"],
+    ['images/mainpage/waify/akame.jpg', "Akame"],
+    ['images/mainpage/waify/mirai.jpg', "Mirai"],
+    ['images/mainpage/waify/Maki.jpg', "Maki"],
+    ['images/mainpage/waify/violet.jpg', "Violet"],
+    ['images/mainpage/waify/hinata.jpg', "Hinata"],
+    ['images/mainpage/waify/kiyoko.jpg', "Kiyoko"],
+    ['images/mainpage/waify/chika.jpg', "Chika"],
+    ['images/mainpage/waify/rem.jpg', "Rem"],
+    ['images/mainpage/waify/tohsaka.jpg', "Rin"],
+    ['images/mainpage/waify/erina.jpg', "Erina"],
+    ['images/mainpage/waify/yuigahama.jpg', "Yui"]
+];
+
+let girls = [
+    {
+        girlImage: "images/gallerypage/megumin.jpg",
+        girlName: "Megumin",
+        girlDescr: "Megumin (めぐみん) is an Arch Wizard of the Crimson Demons in the Fantasy World, and is the first person to join Kazuma's party. She is one of the main characters of the KonoSuba series.",
+    },
+    {
+        girlImage: "images/gallerypage/rem.jpg",
+        girlName: "Rem",
+        girlDescr: "Rem (レム) is a maid in service of Margrave Roswaal L Mathers alongside her sister, Ram. She is one of the main supporting characters of Re:Zero, with major roles in Arc 2, Arc 3, and Arc 7.",
+    },
+    {
+        girlImage: "images/gallerypage/akame.jpeg",
+        girlName: "Akame",
+        girlDescr: "Akame (アカメ) is the titular heroine, alongside Tatsumi, and lead anti-heroine of the manga series Akame ga Kill!, as well as the main protagonist of the prequel manga Akame ga Kill! Zero.",
+    },
+    {
+        girlImage: "images/gallerypage/hinata.jpg",
+        girlName: "Hinata",
+        girlDescr: "Hinata Uzumaki (うずまきヒナタ, Uzumaki Hinata, née Hyūga (日向)) is a kunoichi of Konohagakure. Formerly the heiress of the Hyūga clan, she lost the position upon being deemed unsuited for the responsibilities of leading the clan.",
+    },
+    {
+        girlImage: "images/gallerypage/suzune.jpg",
+        girlName: "Suzune",
+        girlDescr: "Suzune Horikita (堀ほり北きた 鈴すず音ね, Horikita Suzune) is one of the main characters of the You-Zitsu series and the main heroine of the √Horikita spin-off manga. She is known to be the Student Council president of the school on the 1st Year Arc.",
+    },
+    {
+        girlImage: "images/gallerypage/violet.jpeg",
+        girlName: "Violet",
+        girlDescr: "Violet Evergarden (ヴァイオレット・エヴァーガーデン, Vaioretto Evāgāden?) is the protagonist of the Violet Evergarden series. Violet is a former soldier who was enlisted in the Leidenschaftlich army and fought in the war, where she was treated as nothing more than a weapon.",
+    },
+    {
+        girlImage: "images/gallerypage/yuigahama.jpeg",
+        girlName: "Yui",
+        girlDescr: "Yui Yuigahama (由比ヶ浜 結衣 Yuigahama Yui) is a student in Class 2F of Sōbu High School and the first official customer of the Service Club. She later becomes the third member of the Service Club.",
+    },
+    {
+        girlImage: "images/gallerypage/erina.jpg",
+        girlName: "Erina",
+        girlDescr: `Erina Nakiri (薙な切きり えりな, Nakiri Erina) is one of the heroines and was sometimes a rival to Sōma Yukihira in Shokugeki no Soma. Wielding the best palate in the world, "God's Tongue" and a member of the illustrious Nakiri family.`,
+    },
+    {
+        girlImage: "images/gallerypage/chika.jpg",
+        girlName: "Chika",
+        girlDescr: `Chika Fujiwara (藤原 千花ふじわら ちか) is one of the protagonists in the Kaguya-sama wa Kokurasetai series. She was a third-year high school student at Shuchi'in Academy, the secretary of the 67th and 68th Student Council.`,
+    },
+    {
+        girlImage: "images/gallerypage/kiyoko.png",
+        girlName: "Kiyoko",
+        girlDescr: `Kiyoko Shimizu (Japanese: 清水しみず 潔子きよこ Shimizu Kiyoko) was a third-year student at Karasuno High, as well as one of the managers for the Karasuno boy's volleyball team for majority of the series.`,
+    },
+    {
+        girlImage: "images/gallerypage/Maki.jpg",
+        girlName: "Maki",
+        girlDescr: `Maki Zenin (禪ぜん院いん真ま希き Zen'in Maki?) is a major supporting character in Jujutsu Kaisen and one of the main protagonists of its prequel, Jujutsu Kaisen 0: Jujutsu High. She is currently a second-year at Tokyo Jujutsu High.`,
+    },
+    {
+        girlImage: "images/gallerypage/mirai.jpg",
+        girlName: "Mirai",
+        girlDescr: `Mirai Kuriyama (栗山 未来, Kuriyama Mirai ) is the deuteragonist of Kyoukai no Kanata, she's a first-year student and a member of the school's Literature Club. Mirai comes from a bloodline of Spirit World Warriors.`,
+    },
+    {
+        girlImage: "images/gallerypage/rin.jpg",
+        girlName: "Rin",
+        girlDescr: `Rin Tohsaka (遠坂 凛, Tōsaka Rin?) is one of the three main heroines of Fate/stay night who acts as the Master of Archer in the Fifth Holy Grail War. She is the current head of the Tohsaka family and the older sister of Sakura Matou.`,
+    },
+    {
+        girlImage: "images/gallerypage/kikyou.jpeg",
+        girlName: "Kikyou",
+        girlDescr: `Kikyō Kushida (櫛くし田だ 桔き梗きょう, Kushida Kikyō) is one of the main characters of the You-Zitsu series. She is a student of Class 1-D. She is quite popular in her class as well as the whole school as she aims to befriend everyone.`,
+    },
+    {
+        girlImage: "images/gallerypage/mai.jpeg",
+        girlName: "Mai",
+        girlDescr: `Mai Sakurajima (桜島 麻衣, Sakurajima Mai) is the main heroine of the Seishun Buta Yarou wa Bunny Girl Senpai no Yume wo Minai series. She is a notable actress/model and a third year at Minegahara High School.`,
+    },
+    
+];
+
+let usedNums = [];
+let main = document.querySelector("main");
+let randomNum = 0;
+let lastRandomNum = 0;
+let lastNum;
+let generatorBtn = document.getElementById("genBtn");
+let waifyImage = document.querySelector(".generator-image");
+let waifyName = document.querySelector(".generator-heading");
+let mainImage = document.querySelector(".main-image");
+let closeBtn;
+let popupWindow;
+let socialsBtn = document.querySelector(".socials-hover")
+let socialsWindow = document.querySelector(".socials");
+
+socialsBtn.addEventListener("click", function() {
+
+    main.style.pointerEvents = "none";
+    socialsBtn.style.pointerEvents = "none";
+    main.style.opacity = 0.8;
+    socialsWindow.style.display = "flex";
+    let socialsCloseBtn = document.querySelector(".btn-close");
+    socialsCloseBtn.addEventListener("click", function() {
+        socialsWindow.style.display = "none";
+        main.style.opacity = 1;
+        main.style.pointerEvents = "auto";
+        socialsBtn.style.pointerEvents = "auto";
+    })
+
+})
+
+generatorBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    if (usedNums.length == waifus.length) {
+        lastNum = usedNums[waifus.length - 1];
+        console.log(lastNum, "last num");
+        usedNums = [];
+    }
+
+    do {
+        randomNum = Math.trunc(Math.random() * waifus.length);
+    } while(usedNums.includes(randomNum) && usedNums.length != waifus.length || lastNum == randomNum);
+
+    usedNums.push(randomNum);
+
+    waifyImage.src = waifus[randomNum][0];
+    waifyName.textContent = waifus[randomNum][1];
+
+    console.log(usedNums);
+    console.log(randomNum);
+    lastNum = null;
+})
+
+waifyImage.addEventListener("click", function() {
+
+    let currentWaifu;
+
+    for (let object of girls) {
+        if (object.girlName == waifyName.textContent) {
+            currentWaifu = object;
+        }
+    }
+
+    if (currentWaifu) {
+        waifyName.style.display = "none";
+        waifyImage.style.display = "none";
+        mainImage.style.display = "none";
+        generatorBtn.style.display ="none";
+        main.insertAdjacentHTML("afterbegin", `
+        <div class="main-popup-window">
+        <div class="col-lg-4">
+            <button class="btn btn-close btnCloseGenerator"></button>
+            <div class="card text-white mb-3" style="width: 300;">
+                <img src=${currentWaifu.girlImage} class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${currentWaifu.girlName}</h5>
+                    <p class="card-text">${currentWaifu.girlDescr}</p>
+                    <a href="gallery.html" class="btn btn-danger" style="display:block">Go to the gallery!</a>
+                </div>
+            </div>
+        </div>
+        </div>
+        `);
+        popupWindow = document.querySelector(".main-popup-window")
+        closeBtn = document.querySelector(".btnCloseGenerator");
+        closeBtn.addEventListener("click", function() {
+            waifyName.style.display = "block";
+            waifyImage.style.display = "block";
+            mainImage.style.display = "block";
+            generatorBtn.style.display ="block";
+            main.style.opacity = 1;
+            popupWindow.remove();
+        })
+    }
+})
+
+
+
